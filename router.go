d23ef607e8a438d4091ddaebfa833e8bd379e3ff
melvin <melvinn721@gmail.com>
2022-08-27 18:00:59 +1200

    sending signup data to DB, password hashing

--- router.go
+++ router.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/melvin-n/realchat/models"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type appHandler struct {
+	Handler func(http.ResponseWriter, *http.Request) (int, error)
+}
+
+func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	status, err := ah.Handler(w, r)
+	if err != nil {
+		log.Printf("HTTP: %d: %v", status, err)
+	}
+
+	switch status {
+	case http.StatusNotFound:
+		http.NotFound(w, r)
+	case http.StatusBadRequest:
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	case http.StatusInternalServerError:
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	default:
+		http.Error(w, err.Error(), status)
+	}
+}
+
+func router() {
+	r := mux.NewRouter()
+	r.HandleFunc("/signup", signUp).Methods("POST")
+	log.Println("Server listening on port 8080...")
+	http.ListenAndServe(":8080", r)
+}
 
 func signUp(w http.ResponseWriter, r *http.Request) {
 	var newUser models.User
@@ -17,5 +48,31 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 		log.Panic("Unable to parse signUp request")
 	}
 
-	fmt.Println(newUser)
+	if len(newUser.Username) < 4 {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(newUser.HashedPassword), 5)
+	if err != nil {
+		log.Println("Password hashing error")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword := string(password)
+
+	context := context.Background()
+	_, _, err = db.Collection("users").Add(context, models.User{
+		Username:       newUser.Username,
+		Email:          newUser.Email,  //TODO: check email validity
+		HashedPassword: hashedPassword, //TODO: check password validity
+	})
+	if err != nil {
+		log.Fatalf("Unable to add to DB: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK) //TODO: write custom status messages
+	return
 }
