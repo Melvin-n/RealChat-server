15e919b3479ab54425a0d422c40de95aab9619f9
melvin <melvinn721@gmail.com>
2022-09-01 22:18:17 +1200

    added check for duplicate email and username

--- router.go
+++ router.go
@@ -3,39 +3,20 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/melvin-n/realchat/models"
 	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/api/iterator"
 )
-
-type appHandler struct {
-	Handler func(http.ResponseWriter, *http.Request) (int, error)
-}
-
-func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	status, err := ah.Handler(w, r)
-	if err != nil {
-		log.Printf("HTTP: %d: %v", status, err)
-	}
-
-	switch status {
-	case http.StatusNotFound:
-		http.NotFound(w, r)
-	case http.StatusBadRequest:
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	case http.StatusInternalServerError:
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	default:
-		http.Error(w, err.Error(), status)
-	}
-}
 
 func router() {
 	r := mux.NewRouter()
 	r.HandleFunc("/signup", signUp).Methods("POST")
+	r.HandleFunc("/login", login).Methods("POST")
 	log.Println("Server listening on port 8080...")
 	http.ListenAndServe(":8080", r)
 }
@@ -58,11 +39,15 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
 	hashedPassword := string(password)
 
-	context := context.Background()
-	_, _, err = db.Collection("users").Add(context, models.User{
+	ctx := context.Background()
+
+	//search if user or email already exists
+	checkForDuplicates(ctx, "users", "Username", newUser.Username)
+	checkForDuplicates(ctx, "users", "Email", newUser.Email)
+
+	_, _, err = db.Collection("users").Add(ctx, models.User{
 		Username:       newUser.Username,
 		Email:          newUser.Email,  //TODO: check email validity
 		HashedPassword: hashedPassword, //TODO: check password validity
@@ -75,4 +60,40 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK) //TODO: write custom status messages
 	return
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var userDetails models.User
+	err := json.NewDecoder(r.Body).Decode(&userDetails)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Panic("Unable to parse signUp request")
+	}
+	fmt.Println(userDetails.Username)
+	ctx := context.Background()
+	iter := db.Collection("users").Where("Username", "==", userDetails.Username).Documents(ctx)
+
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		fmt.Println(doc.Data())
+	}
+
+}
+
+func checkForDuplicates(ctx context.Context, collection, field, value string) {
+	iter := db.Collection(collection).Where(field, "==", value)
+	matches, err := iter.Documents(ctx).GetAll()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if len(matches) > 0 {
+		log.Fatalf("%s is already attached to an account", field)
+	}
 }
